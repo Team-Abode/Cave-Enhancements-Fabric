7df0a47614a564307408779782150661eec75e59
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2022-09-15 00:23:36 +0100

    Thrown Goop

--- src/main/java/com/exdrill/cave_enhancements/item/GoopItem.java
+++ src/main/java/com/exdrill/cave_enhancements/item/GoopItem.java
@@ -1,15 +1,21 @@
 package com.exdrill.cave_enhancements.item;
 
+import com.exdrill.cave_enhancements.entity.goop.ThrownGoop;
 import com.exdrill.cave_enhancements.registry.ModBlocks;
 import com.exdrill.cave_enhancements.registry.ModSounds;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -24,6 +30,26 @@ public class GoopItem extends Item {
     public GoopItem(Properties properties) {
         super(properties);
     }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        ItemStack itemStack = player.getItemInHand(usedHand);
+        level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), ModSounds.ITEM_GOOP_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+        if (!level.isClientSide) {
+            ThrownGoop thrownGoop = new ThrownGoop(level, player);
+            thrownGoop.setItem(itemStack);
+            thrownGoop.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+            level.addFreshEntity(thrownGoop);
+        }
+
+        player.awardStat(Stats.ITEM_USED.get(this));
+        if (!player.getAbilities().instabuild) {
+            itemStack.shrink(1);
+        }
+
+        return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
+    }
+
 
     @Override
     public InteractionResult useOn(UseOnContext context) {
--- src/main/java/com/exdrill/cave_enhancements/registry/ModEntities.java
+++ src/main/java/com/exdrill/cave_enhancements/registry/ModEntities.java
@@ -7,6 +7,7 @@ import com.exdrill.cave_enhancements.entity.dripstone_tortoise.DripstonePike;
 import com.exdrill.cave_enhancements.entity.dripstone_tortoise.DripstoneTortoise;
 import com.exdrill.cave_enhancements.entity.goop.BigGoopDripProjectile;
 import com.exdrill.cave_enhancements.entity.goop.Goop;
+import com.exdrill.cave_enhancements.entity.goop.ThrownGoop;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
@@ -26,6 +27,8 @@ public class ModEntities {
     public static final EntityType<DripstonePike> DRIPSTONE_PIKE = Registry.register(Registry.ENTITY_TYPE, new ResourceLocation(CaveEnhancements.MODID, "dripstone_pike"), FabricEntityTypeBuilder.create(MobCategory.MISC, DripstonePike::new).dimensions(EntityDimensions.fixed(0.3F, 0.3F)).build());
 
     public static final EntityType<HarmonicArrow> HARMONIC_ARROW = Registry.register(Registry.ENTITY_TYPE, new ResourceLocation(CaveEnhancements.MODID, "harmonic_arrow"), FabricEntityTypeBuilder.<HarmonicArrow>create(MobCategory.MISC, HarmonicArrow::new).dimensions(EntityDimensions.fixed(0.5F, 0.5F)).build());
+
+    public static final EntityType<ThrownGoop> THROWN_GOOP = Registry.register(Registry.ENTITY_TYPE, new ResourceLocation(CaveEnhancements.MODID, "thrown_goop"), FabricEntityTypeBuilder.<ThrownGoop>create(MobCategory.MISC, ThrownGoop::new).dimensions(EntityDimensions.fixed(0.25F, 0.25F)).build());
 
     public static void init() {
         FabricDefaultAttributeRegistry.register(GOOP, Goop.createGoopAttributes());
