6d9141523a9bd449feeedefd2a0b103f10409731
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-11-30 21:19:55 -0600

    Fix: Receiver Block

--- src/main/java/com/teamabode/cave_enhancements/block/entity/ReceiverBlockEntity.java
+++ src/main/java/com/teamabode/cave_enhancements/block/entity/ReceiverBlockEntity.java
@@ -10,37 +10,40 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class ReceiverBlockEntity extends BlockEntity {
 
-    public int poweredTicks = 0;
+    public int timePoweredTicks = 0;
+    public int output;
 
     public ReceiverBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.RECEIVER_BLOCK, pos, state);
     }
 
     public void tick(Level world, BlockPos pos, BlockState state) {
-        ReceiverBlock block = (ReceiverBlock) state.getBlock();
-        int maxPower = block.getMaxPower();
+        int requiredPowerDurationTicks = ((ReceiverBlock) state.getBlock()).getRequiredPowerDurationTicks();
 
         if (state.getValue(ReceiverBlock.POWERED)) {
-            this.poweredTicks++;
-        } else {
-            world.setBlockAndUpdate(pos, state.setValue(ReceiverBlock.CAN_PASS, false));
-            poweredTicks = 0;
+            if(timePoweredTicks < requiredPowerDurationTicks) timePoweredTicks++;
+        }else{
+            timePoweredTicks = 0;
         }
-        if (poweredTicks == maxPower) {
-            world.setBlockAndUpdate(pos, state.setValue(ReceiverBlock.CAN_PASS, true));
-        }
+
+        world.setBlockAndUpdate(pos, state.setValue(ReceiverBlock.CAN_PASS, timePoweredTicks == requiredPowerDurationTicks));
     }
 
     @Override
     protected void saveAdditional(CompoundTag nbt) {
-        nbt.putInt("PoweredTicks", this.poweredTicks);
-        this.setChanged();
+        nbt.putInt("PoweredTicks", this.timePoweredTicks);
+        nbt.putInt("OutputSignal", this.output);
+
+        setChanged();
+
         super.saveAdditional(nbt);
     }
 
     @Override
     public void load(CompoundTag nbt) {
-        this.poweredTicks = nbt.getInt("PoweredTicks");
+        timePoweredTicks = nbt.getInt("PoweredTicks");
+        output = nbt.getInt("OutputSignal");
+
         super.load(nbt);
     }
 
